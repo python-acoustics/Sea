6eb9b6ad8acb12d89629894338e01b192e7c9089
Frederik Rietdijk <fridh@fridh.nl>
2013-04-04 09:44:21 +0530

    Remove baseclasses.py file.

--- Sea/model/baseclasses.py
+++
@@ -1,4 +0,0 @@
-"""
-This module contains abstract base classes from which all components, subsystems, couplings and excitations are ultimately derived of.
-The properties these items have in common can be found in these abstract classes.
-"""
